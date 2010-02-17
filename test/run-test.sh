c3d35d190de029174cf57c6c65e8be9735a0f725
MIURA Yasuyuki <kokosabu@gmail.com>
2010-02-17 22:33:37 +0900

    setup GNU build system

---
+++ test/run-test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export BASE_DIR="`dirname $0`"
+
+if test -z "$NO_MAKE"; then
+    make -C $BASE_DIR/../ > /dev/null || exit 1
+fi
+
+if test -z "$CUTTER"; then
+    CUTTER="`make -s -C $BASE_DIR echo-cutter`"
+fi
+
+case `uname` in
+    CYGWIN*)
+        PATH="$top_dir/src/.libs:$PATH"
+        ;;
+    *)
+        :
+        ;;
+esac
+
+$CUTTER -s $BASE_DIR "$@" $BASE_DIR
